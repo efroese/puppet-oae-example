9fd98c76bcb711d7ad4df9b087db4fd1b2ef186f
Marc Fournier <marc.fournier@camptocamp.com>
2009-03-02 17:58:27 +0100

    selinux: ajout fact pour vérifier si selinux.rb match rev 0e467869

---
+++ plugins/facter/issue1963fixed.rb
@@ -0,0 +1,33 @@
+Facter.add("issue1963fixed") do
+  setcode do
+
+    sha1sum = "48e6284d2966b61574bdb233d9826fc6d1c079a3"
+
+    begin
+      Facter.rubysitedir
+    rescue
+      Facter.loadfacts()
+    end
+
+    rubysitedir = Facter.value('rubysitedir')
+    file = rubysitedir + "/puppet/util/selinux.rb"
+
+    if FileTest.exists?(file) and FileTest.file?(file)
+
+      require 'digest/sha1'
+      hash_func = Digest::SHA1.new
+      open(file, "r") do |io|
+        while (!io.eof)
+          readBuf = io.readpartial(1024)
+          hash_func.update(readBuf)
+        end
+      end
+
+      hash_func.hexdigest == sha1sum
+
+    else
+      false
+    end
+
+  end
+end

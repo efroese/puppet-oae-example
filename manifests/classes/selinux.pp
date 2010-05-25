3c764d9d63edf26fb2d6a461833164e57225d19b
Marc Fournier <marc.fournier@camptocamp.com>
2010-05-25 12:21:45 +0200

    selinux: removed legacy code, this module will no longer work with puppet < 0.24.8

--- manifests/classes/selinux.pp
+++ manifests/classes/selinux.pp
@@ -1,12 +1,3 @@
-# Rationale:
-# - puppet selinux code kicks in automatically as soon as ruby bindings are
-#   available
-# - http://projects.reductivelabs.com/issues/1963 must be fixed before selinux
-#   code gets run
-# - selinux ruby bindings are available only on newer distributions
-# - for legacy distros, http://github.com/twpayne/libselinux-ruby-puppet/ can
-#   be used.
-
 class selinux::base {
 
   # required to build custom policy modules.
@@ -42,47 +33,8 @@ class selinux::base {
 
   }
 
-
-  case $puppetversion {
-
-    "0.24.7": {
-
-      if $issue1963fixed == "yes" {
-
-        package { "$rubypkg_alias":
-          ensure => present,
-          alias => "selinux-ruby-bindings",
-        }
-
-        file { "/tmp/issue1963.patch": ensure => absent }
-
-      }
-      else {
-
-        file { "/tmp/issue1963.patch":
-          source => "puppet:///selinux/changeset_r0e467869f4d427a8c42e1c2ff6a0bb215f288b09.diff",
-          ensure => present,
-        }
-
-        exec { "patch selinux.rb with issue1963.patch":
-          command => "cat /tmp/issue1963.patch | patch -p0 ${rubysitedir}/puppet/util/selinux.rb",
-          unless => "grep -q 'File.open(\"/proc/mounts\", NONBLOCK)' ${rubysitedir}/puppet/util/selinux.rb",
-          require => File["/tmp/issue1963.patch"],
-        }
-      }
-    }
-
-    "0.24.6","0.24.5","0.24.4": {
-      # no ruby-selinux implementation in older versions
-    }
-
-    # TODO: /0\.2(4\.8|5\..*)/
-    "0.24.8", "0.24.9", "0.25.0", "0.25.1", "0.25.2", "0.25.3", "0.25.4": {
-      package { "$rubypkg_alias":
-        ensure => present,
-        alias => "selinux-ruby-bindings",
-      }
-    }
-
+  package { "$rubypkg_alias":
+    ensure => present,
+    alias => "selinux-ruby-bindings",
   }
 }

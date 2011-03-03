8d060b5222ec8848894e226775b5f65af9d85a24
Kit Plummer <kitplummer@gmail.com>
2011-03-02 16:24:37 -0800

    support for username and transfer of .wgetrc for password

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,3 +29,29 @@ define wget::fetch($source,$destination) {
 		}
 	}
 }
+
+################################################################################
+# Definition: wget::authfetch
+#
+# This class will download files from the internet.  You may define a web proxy
+# using $http_proxy if necessary. Username must be provided. And the user's
+# password must be stored in the password variable within the .wgetrc file.
+#
+################################################################################
+define wget::authfetch($source,$destination,$user,$timeout="0") {
+	if $http_proxy {
+		exec { "wget-$name":
+			command => "/usr/bin/wget --user=$user --output-document=$destination $source",
+      timeout => "$timeout",
+			creates => "$destination",
+			environment => [ "HTTP_PROXY=$http_proxy", "http_proxy=$http_proxy" ],
+		}
+  } else {
+		exec { "wget-$name":
+			command => "/usr/bin/wget --user=$user --output-document=$destination $source",
+      timeout => "$timeout",
+			creates => "$destination",
+		}
+	}
+}
+

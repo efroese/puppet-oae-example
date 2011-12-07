3d9d797bbe5c56ecff619932b95a34e12a3264cc
Erik Froese <erik.froese@gmail.com>
2011-12-07 12:40:00 -0500

    First stab at a preview processor in puppet.

---
+++ modules/preview_processor/manifests/init.pp
@@ -0,0 +1,87 @@
+class preview_processor {
+
+    # Present in the base CentOS repositories
+    $common_packages = ['cpp', 'curl-devel', 'poppler-utils', 'rubygems']
+
+    # From rpmforge
+    $docsplit_packages = ['pdftk', 'tesseract']
+    package { $docsplit_packages: ensure => installed }
+
+    if ($operatingsystem == 'CentOS' || $operatingsystem == 'RedHat') && ($lsbmajdistrelease == '5'){
+        # CentOS needs updated ImageMagick and Ruby packages
+        $centos_packages = ['ImageMagick-6.4.9-10', 'ImageMagick-devel-6.4.9-10', 'ruby1.9.2p0-1.9.2p0-1']
+        package { $centos_packages: ensure => installed }
+    }
+
+    if $operatingsystem == 'Fedora' {
+        # Fedora can use the base packages.
+        $fedora_packages = ['ImageMagick', 'ImageMagick-devel']
+        package { $fedora_packages: ensure => installed }
+    }
+
+    ###########################################################################
+    # Run the OpenOffice service to convert docs
+    $ooo_packages = ['openoffice.org-core', 
+            'openoffice.org-javafilter', 
+            'openoffice.org-headless', 
+            'openoffice.org-writer.x86_64',
+        ]
+
+    package { $ooo_packages: 
+        ensure => installed,
+        notify => Service['soffice']
+    }
+        
+    # Create Link /usr/lib/openoffice
+    file { '/usr/lib/openoffice':
+        ensue => link,
+        target => '/usr/lib64/openoffice.org3'
+        require => Packages['openoffice.org-core'],
+    }
+    
+    file { '/etc/init.d/soffice':
+        source => 'puppet://modules/preview_processor/soffice.sh'.
+        owner  => root,
+        group  => root,
+        mode   => 755,
+    }
+    
+    service { 'soffice':
+        ensure => running,
+    }
+    
+    ###########################################################################
+    # Ruby Gems for the preview_processor.rb script
+    $ruby_gems = ['json', 'docsplit', 'rmagick']
+
+    package { $ruby_gems: 
+        provider => 'gem',
+        ensure => installed,
+        require => Package['rubygems']
+    }
+
+    ###########################################################################
+    # A place for PP-specific files
+    file { '/usr/local/share/preview_processor':
+        ensure => directory,
+        owner  => root,
+        group  => root,
+        mode   => 754,
+    }
+
+    ###########################################################################
+    # Drop the script for the cron job
+    file { '/usr/local/share/preview_processor/run_preview_processor.sh':
+        source => 'puppet://modules/preview_processor/run_preview_processor.sh'.
+        owner  => root,
+        group  => root,
+        mode   => 755,
+    }
+    
+    cron { 'run_preview_processor':
+        command => '/usr/local/share/preview_processor/run_preview_processor.sh',
+        ensure => present,
+        user => root,
+        minute => *,
+    }
+}
---
+++ nodes.pp
@@ -0,0 +1,9 @@
+centos5-oae {
+    include git
+    include preview_processor
+}
+
+centos6-oae {
+    include git
+    include preview_processor
+}

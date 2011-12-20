86cc3c1479ef29c4873ec9f4e891cda40494bd14
Erik Froese <erik.froese@gmail.com>
2011-12-20 08:30:14 -0700

    Fix config dir creation so it only happens once.

--- modules/oae/manifests/init.pp
+++ modules/oae/manifests/init.pp
@@ -1,6 +1,6 @@
 class oae {
 
-    Class['aoe'] -> Class['oae::params']
+    Class['oae'] -> Class['oae::params']
 
     define sling_config($dirname, $pid, $config){
 
@@ -9,6 +9,7 @@ class oae {
         if !defined(Exec["mkdir_${sling_config}/${dirname}"]) {
             exec { "mkdir_${sling_config}/${dirname}":
                 command => "mkdir -p ${sling_config}/${dirname}",
+                creates => "${sling_config}/${dirname}",
                 require => Class['oae-app'],
             }
         }
@@ -16,7 +17,8 @@ class oae {
         if !defined(Exec["chown_${sling_config}/${dirname}"]) {
             exec { "chown_${sling_config}/${dirname}":
                 command => "chown -R ${oae::params::user}:${oae::params::group} ${sling_config}/${dirname}",
-                require => Class['oae-app'],
+                require => Exec["mkdir_${sling_config}/${dirname}"],
+                unless  => "[ `stat --printf='%U' ${sling_config}/${dirname}` == '${$oae::params::user}' ]"
             }
         }
 

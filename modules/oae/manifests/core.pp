d100a91ef26cb7734dab77c6293a2a5e0a767ad6
Erik Froese <erik.froese@gmail.com>
2011-12-20 20:13:43 -0700

    Add $ to variable names. woops.

--- modules/oae/manifests/core.pp
+++ modules/oae/manifests/core.pp
@@ -6,10 +6,10 @@ class oae::core {
         dirname => "org/sakaiproject/nakamura/lite/storage/jdbc",
         pid     => '"org.sakaiproject.nakamura.lite.storage.jdbc.JDBCStorageClientPool"',
         config => {
-            'jdbc-driver' => oae::params::sparsedriver,
-            'jdbc-url'    => oae::params::sparseurl,
-            'username'    => oae::params::sparseuser,
-            'password'    => oae::params::sparsepass,
+            'jdbc-driver' => $oae::params::sparsedriver,
+            'jdbc-url'    => $oae::params::sparseurl,
+            'username'    => $oae::params::sparseuser,
+            'password'    => $oae::params::sparsepass,
         }
     }
     

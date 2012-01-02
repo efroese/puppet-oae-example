c568c595d178d78a6a51740706c0f7b861ad0453
Erik Froese <erik.froese@gmail.com>
2012-01-02 11:41:44 -0500

    Paramterize the oae::core class and remove its settings from oae::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,7 @@
 class oae::params {
 
+    ###########################################################################
+    # Basic system stuff
     $user = 'sakaioae'
     $group = 'sakaioae'
 
@@ -12,18 +14,13 @@ class oae::params {
         mode   => 750,
     }
 
+    ###########################################################################
+    # For using a Highly available cluster of Apache servers with a virtual IP
     $virtual_ip         = '192.168.1.40'
     $virtual_ip_netmask = '255.255.255.0'
     $http_hosts         = ['centos5-oae-lb1.localdomain', 'centos5-oae-lb2.localdomain']
 
-    ##############################################
-    # SparseMapContent (Core)
-    $sparseurl  = "jdbc:mysql://192.168.1.250:3306/nakamura?autoReconnectForPools\\=true"
-    $sparsedriver = "com.mysql.jdbc.Driver"
-    $sparseuser = 'nakamura'
-    $sparsepass = 'ironchef'
-
-    ##############################################
+    ###########################################################################
     # ServerProctectionService
     $serverprotectsec = 'thisisasecret'
     $http_name = 'centos5-oae.localdomain'

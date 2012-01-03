7018d6d86b8c07a70a627ca5b7a7aafb5fca1acc
Erik Froese <erik.froese@gmail.com>
2012-01-02 23:50:35 -0500

    Moved these vars to nodes.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,15 +13,4 @@ class oae::params {
         group  => $oae::params::group,
         mode   => 750,
     }
-
-    ###########################################################################
-    # For using a Highly available cluster of Apache servers with a virtual IP
-    $virtual_ip         = '192.168.1.40'
-    $virtual_ip_netmask = '255.255.255.0'
-    $http_hosts         = ['centos5-oae-lb1.localdomain', 'centos5-oae-lb2.localdomain']
-
-    ###########################################################################
-    # ServerProctectionService
-    $serverprotectsec = 'thisisasecret'
-    $http_name = 'centos5-oae.localdomain'
 }

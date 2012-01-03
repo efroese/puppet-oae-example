00123e2c9bd17d0f15380168fc9ab73e1a9787b9
Erik Froese <erik.froese@gmail.com>
2012-01-03 00:05:37 -0500

    Add docs.

--- manifests/app/server.pp
+++ manifests/app/server.pp
@@ -1,9 +1,37 @@
-class oae::app::server($version_oae,
-                $downloaddir, $jarfile,
-                $javamemorymax, $javapermsize) {
+# = Class: oae::app::server
+#
+# This class installs an OAE app server and runs it.
+#
+# == Parameters:
+#
+# $version_oae::   The version of OAE to run
+#
+# $downloaddir::   The URL to the download directory where the app jar lives
+#
+# $jarfile::       The name of the jar file to download
+#
+# $javamemorymax:: The max java heap size
+#
+# $javapermsize::  The max java perm gen space
+#
+# == Actions:
+#   Install a Sakai OAE app jar and start it up
+#
+# == Sample Usage:
+#
+#   class { 'oae::app::server':
+#     version_oae   => '1.1'
+#     downloaddir   => 'http://192.168.1.124/jars/',
+#     jarfile       => 'org.sakaiproject.nakamura.app-1.1-mysql.jar',
+#     javamemorymax => 512,
+#     javapermsize  => 256,
+#   }
+#
+class oae::app::server($version_oae, $downloaddir, $jarfile,
+                        $javamemorymax, $javapermsize) {
 
     include oae::app::setup
-    
+
     Class['oae::app::setup'] -> Class['oae::app::server']
 
     file { "${oae::params::basedir}/sling/nakamura.properties":
@@ -12,6 +40,7 @@ class oae::app::server($version_oae,
         group   => $oae::params::user,
         mode    => '0644',
         source  => "puppet:///modules/oae/nakamura.properties",
+        notify  => Service['sakaioae']
     }
 
     $jar_dest = "${oae::params::basedir}/jars/${jarfile}"

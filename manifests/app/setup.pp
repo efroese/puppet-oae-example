ee4668c11dbbdfa8907e185c779388b3a95493d0
Erik Froese <erik.froese@gmail.com>
2012-01-04 14:35:28 -0500

    Trim the number of directories to create.

--- manifests/app/setup.pp
+++ manifests/app/setup.pp
@@ -9,14 +9,11 @@ class oae::app::setup {
 
     $sling_dir  = "${oae::params::basedir}/sling"
     $config_dir = "${sling_dir}/config"
-    $solr_dir   = "${sling_dir}/solr"
 
     # TODO this needs to become a mount
     $sparse_store_dir = "${oae::params::basedir}/store"
 
-    $app_dirs = [ $oae::params::basedir, $jar_dir, $sling_dir, $config_dir, $log_dir, $solr_dir, $sparse_store_dir ]
-
-    file { [ $jar_dir, $sling_dir, $config_dir, $log_dir, $solr_dir, $sparse_store_dir ]:
+    file { [ $jar_dir, $sling_dir, $config_dir, $log_dir, $sparse_store_dir ]:
         ensure => directory,
         owner  => $oae::params::user,
         group  => $oae::params::user,

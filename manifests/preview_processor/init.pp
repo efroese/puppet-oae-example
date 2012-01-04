aa6bcb04ac6b98bb1fc422d24aa5105075f7e3f9
Erik Froese <erik.froese@gmail.com>
2012-01-04 11:26:34 -0500

    Use the correct params and clone nakamura.

--- manifests/preview_processor/init.pp
+++ manifests/preview_processor/init.pp
@@ -1,19 +1,34 @@
-class oae::preview_processor::init ($oae_user="sakaioae", $basedir="/usr/local/sakaioae") {
+class oae::preview_processor::init ($nakamura_git, $nakamura_tag) {
 
-    file { "${basedir}/bin":
+    Class['oae::params'] -> Class['oae::preview_processor::init']
+
+    file { "${oae::params::basedir}/bin":
         ensure => directory,
-        owner  => $oae_user,
-        group  => $oae_user,
+        owner  => $oae::params::user,
+        group  => $oae::params::group,
         mode   => 750,
     }
 
     case $operatingsystem {
         /RedHat|CentOS/:   { include oae::preview_processor::redhat }
-        /Debian|Ubuntu/:  { include oae::preview_processor::debian }
+        /Debian|Ubuntu/:   { include oae::preview_processor::debian }
     }
 
     include oae::preview_processor::openoffice
     include oae::preview_processor::gems
+
+    exec { "clone nakamura":
+        command => "git clone ${nakamura_git} ${oae::params::basedir}/nakamura",
+        creates => "${oae::params::basedir}/nakamura",
+        require => Package['git'],
+        notify  => Exec['checkout nakamura tag'],
+    }
+
+    exec { "checkout nakamura tag":
+        command => "cd ${oae::params::basedir}/nakamura && git checkout ${nakamura_tag}",
+        require => [ Package['git'], Exec['clone nakamura'], ],
+        refreshonly => true,
+    }
 
     ###########################################################################
     # Drop the script for the cron job

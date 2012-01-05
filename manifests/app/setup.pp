21a72e4a32bbbe306c124bea166da128433a7928
Erik Froese <erik.froese@gmail.com>
2012-01-05 14:48:31 -0500

    Create sling/config/org/apache writable by the server user.

--- manifests/app/setup.pp
+++ manifests/app/setup.pp
@@ -26,4 +26,20 @@ class oae::app::setup {
         group   => $oae::params::user,
         target  => $log_dir,
     }
+
+    file { "${config_dir}/org":
+        ensure => directory,
+        owner  => root,
+        group  => root,
+        mode   => 0644,
+    }
+
+    # The sling logging bundle causes the config admin service to write configs under org/apache/sling/...
+    # TODO: we may want to tighten up the perms in this directory to avoid runtime reconfigurations
+    file { "${config_dir}/org/apache":
+        ensure => directory,
+        owner  => $oae::params::user,
+        group  => $oae::params::user,
+        mode   => 0644,
+    }
 }

8cf496b59650a98a679b49dfa64efee089ed68d1
Erik Froese <erik.froese@gmail.com>
2012-01-06 13:00:37 -0500

    Deliver a patch to docsplit for psdf files with non-utf8 in the info.

--- manifests/preview_processor/gems.pp
+++ manifests/preview_processor/gems.pp
@@ -15,23 +15,62 @@ class oae::preview_processor::gems {
         }
     }
 
-    if $operatingsystem == 'CentOS' and $lsbmajdistrelease == '5' {
-
-        opt_gem { 'curb': version => '0.7.15' }
-        opt_gem { 'docsplit': }
-        opt_gem { 'json': }
-        opt_gem { 'rmagick': }
-        opt_gem { 'getopt': }
-        opt_gem { 'daemons': }
-
-    } else {
-
-        # Ruby Gems for the preview_processor.rb script
-        $ruby_gems = ['curb', 'json', 'docsplit', 'rmagick', 'getopt', 'daemons']
-
-        package { $ruby_gems:
-            provider => 'gem',
+    define gem($version) {
+        package { $name:
             ensure => installed,
+            provider => 'gem',
+            version => $version
         }
+    }
+
+    if $operatingsystem == 'CentOS' and $lsbmajdistrelease == '5' {
+        opt_gem { 'curb':     version => '0.7.15' }
+        opt_gem { 'docsplit': version => '0.6.3' }
+        opt_gem { 'json':     version => '1.6.3' }
+        opt_gem { 'rmagick':  version => '2.13.1' }
+        opt_gem { 'getopt':   version => '1.4.1' }
+        opt_gem { 'daemons':  version => '1.1.5' }
+
+        exec { 'patch-docsplit':
+            command => "patch -p0 < ${oae::params::basedir}/patches/info_extractor.rb.patch info_extractor.rb",
+            cwd     => "/opt/local/lib64/ruby/gems/1.9.1/gems/docsplit-0.6.3/lib/docsplit/",
+            require => File["${oae::params::basedir}/patches/info_extractor.rb.patch"],
+        }
+
+    } 
+    else {
+        gem { 'curb':     version => '0.7.15' }
+        gem { 'docsplit': version => '0.6.3' }
+        gem { 'json':     version => '1.6.3' }
+        gem { 'rmagick':  version => '2.13.1' }
+        gem { 'getopt':   version => '1.4.1' }
+        gem { 'daemons':  version => '1.1.5' }
+        
+        exec { 'patch-docsplit': 
+            command => "patch -p0 < ${oae::params::basedir}/patches/info_extractor.rb.patch info_extractor.rb",
+            cwd     => "/usr/lib/ruby/gems/1.8/gems/docsplit-0.6.3/lib/docsplit",
+            require => File["${oae::params::basedir}/patches/info_extractor.rb.patch"],
+        }
+    }
+
+    file { "${oae::params::basedir}/patches":
+        ensure => directory,
+        owner  => $oae::params::user,
+        group  => $oae::params::group,
+        mode   => 0750,
+    }
+
+    file { "${oae::params::basedir}/patches/info_extractor.rb.patch":
+        ensure => present,
+        owner  => $oae::params::user,
+        group  => $oae::params::group,
+        mode   => 0640,
+        content => "0a1,2
+> require 'iconv'
+> 
+22c24
+<       result = `#{cmd}`.chomp
+---
+>       result = Iconv.conv('ASCII//IGNORE', 'UTF8', `#{cmd}`.chomp)"
     }
 }

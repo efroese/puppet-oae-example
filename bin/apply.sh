fb4fa06ea22986905fc077531103d621edbaf1e2
Erik Froese <erik.froese@gmail.com>
2012-01-10 22:40:15 -0700

    Remove verbose and debug. Add comments.

--- bin/apply.sh
+++ bin/apply.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-puppet apply --modulepath modules site.pp --verbose --debug $@
+#
+# apply.sh - Erik Froese <erik@hallwaytech.com>
+#
+# Run puppet with the correct module path.
+#
+# For more info pass the --debug or --verbose flags.
+#
+puppet apply --modulepath modules site.pp $@

1d370d88a360dda85051abb2eaf8075974e26545
Erik Froese <erik.froese@gmail.com>
2012-01-13 09:15:46 -0500

    Don't create the store dir.

--- manifests/app/setup.pp
+++ manifests/app/setup.pp
@@ -10,10 +10,7 @@ class oae::app::setup {
     $sling_dir  = "${oae::params::basedir}/sling"
     $config_dir = "${sling_dir}/config"
 
-    # TODO this needs to become a mount
-    $sparse_store_dir = "${oae::params::basedir}/store"
-
-    file { [ $jar_dir, $sling_dir, $config_dir, $log_dir, $sparse_store_dir]:
+    file { [ $jar_dir, $sling_dir, $config_dir, $log_dir,]:
         ensure => directory,
         owner  => $oae::params::user,
         group  => $oae::params::user,

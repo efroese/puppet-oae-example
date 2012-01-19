7e4be7aa27902ef070bc148e67af4c6f2024d288
Erik Froese <erik.froese@gmail.com>
2012-01-19 09:37:26 -0500

    More comment and readability betterness!

--- nodes-rsmart-staging.pp
+++ nodes-rsmart-staging.pp
@@ -10,10 +10,7 @@
 #
 # Apache load balancer
 #
-
 node 'staging-apache1.academic.rsmart.local' inherits oaenode {
-
-    $http_name = $localconfig::http_name
 
     class { 'apache::ssl': }
 
@@ -21,13 +18,16 @@ node 'staging-apache1.academic.rsmart.local' inherits oaenode {
     apache::module { 'headers': }
     apache::module { 'deflate': }
 
-    # Simple vhost to redirect to 443
-    apache::vhost { "${http_name}:80":
+    # http://staging.academic.rsmart.com to redirects to 443
+    apache::vhost { "${localconfig::http_name}:80":
         template => 'localconfig/vhost-80.conf.erb',
     }
 
-    # Server trusted content on 443
-    apache::vhost-ssl { "${http_name}:443":
+    ###########################################################################
+    # https://staging.academic.rsmart.com:443
+
+    # Serve the OAE app (trusted content) on 443
+    apache::vhost-ssl { "${localconfig::http_name}:443":
         sslonly  => true,
         cert     => "/etc/pki/tls/certs/rsmart.com.crt",
         certkey  => "/etc/pki/tls/private/rsmart.com.key",
@@ -35,9 +35,9 @@ node 'staging-apache1.academic.rsmart.local' inherits oaenode {
         template  => 'localconfig/vhost-443.conf.erb',
     }
 
-    # Server pool for trusted content
+    # Balancer pool for trusted content
     apache::balancer { "apache-balancer-oae-app":
-        vhost      => "${http_name}:443",
+        vhost      => "${localconfig::http_name}:443",
         location   => "/",
         locations_noproxy => ['/server-status', '/balancer-manager'],
         proto      => "http",
@@ -47,9 +47,9 @@ node 'staging-apache1.academic.rsmart.local' inherits oaenode {
         template   => 'localconfig/balancer-trusted.erb',
     }
 
-    # Server pool for CLE
+    # Balancer pool for CLE traffic
     apache::balancer { "apache-balancer-cle":
-        vhost      => "${http_name}:443",
+        vhost      => "${localconfig::http_name}:443",
         proto      => "ajp",
         members    => $localconfig::apache_cle_lb_members,
         params     => [ "timeout=300", "loadfactor=100" ],
@@ -57,11 +57,13 @@ node 'staging-apache1.academic.rsmart.local' inherits oaenode {
         template   => 'localconfig/balancer-cle.erb',
     }
 
+    ###########################################################################
+    # https://staging.academic.rsmart.com:8443
+
     # Serve untrusted content from 8443
-    # The puppet module takes care of 80 and 443 automatically.
     apache::listen { "8443": }
     apache::namevhost { "*:8443": }
-    apache::vhost-ssl { "${http_name}:8443": 
+    apache::vhost-ssl { "${localconfig::http_name}:8443":
         sslonly  => true,
         sslports => ['*:8443'],
         cert     => "/etc/pki/tls/certs/rsmart.com.crt",
@@ -70,15 +72,18 @@ node 'staging-apache1.academic.rsmart.local' inherits oaenode {
         template  => 'localconfig/vhost-8443.conf.erb',
     }
 
-    # Server pool for untrusted content
+    # Balancer pool for untrusted content
     apache::balancer { "apache-balancer-oae-app-untrusted":
-        vhost      => "${http_name}:8443",
+        vhost      => "${localconfig::http_name}:8443",
         location   => "/",
         proto      => "http",
         members    => $localconfig::apache_lb_members_untrusted,
         params     => ["retry=20", "min=3", "flushpackets=auto"],
         standbyurl => $localconfig::apache_lb_standbyurl,
     }
+
+    ###########################################################################
+    # Apache global config
 
     file { "/etc/httpd/conf.d/traceenable.conf":
         owner => root,
@@ -94,8 +99,6 @@ node 'staging-apache1.academic.rsmart.local' inherits oaenode {
 #
 node oaeappnode inherits oaenode {
 
-    $http_name = $localconfig::http_name
-
     class { 'oae::app::server':
         jarsource      => $localconfig::jarsource,
         jarfile        => $localconfig::jarfile,
@@ -106,7 +109,10 @@ node oaeappnode inherits oaenode {
         setenv_template => 'localconfig/setenv.sh.erb',
     }
     
-    # NFS mounted shated storage for content bodies
+    ###########################################################################
+    # Storage
+
+    # NFS mounted shared storage for content bodies
     file { $localconfig::storedir:
         ensure => directory,
         owner => root,
@@ -125,14 +131,17 @@ node oaeappnode inherits oaenode {
     oae::app::server::sling_config {
         "org.sakaiproject.nakamura.lite.storage.jdbc.JDBCStorageClientPool":
         config => {
-            'jdbc-driver' => $localconfig::db_driver,
-            'jdbc-url'    => $localconfig::db_url,
-            'username'    => $localconfig::db_user,
-            'password'    => $localconfig::db_password,
+            'jdbc-driver'      => $localconfig::db_driver,
+            'jdbc-url'         => $localconfig::db_url,
+            'username'         => $localconfig::db_user,
+            'password'         => $localconfig::db_password,
             'long-string-size' => 16384,
             'store-base-dir'   => $localconfig::storedir,
         }
     }
+
+    ###########################################################################
+    # Security
 
     # Separates trusted vs untrusted content.
     oae::app::server::sling_config {
@@ -140,14 +149,29 @@ node oaeappnode inherits oaenode {
         config => {
             'disable.protection.for.dev.mode' => false,
             'trusted.hosts'  => [
-                "localhost\\ \\=\\ https://localhost:8443", 
-                "${http_name}\\ \\=\\ https://${http_name}:8443",
+                "localhost:8080\\ \\=\\ http://localhost:8081",
+                "${hostname}:8080\\ \\=\\ http://${hostname}:8081",
+                "${localconfig::http_name}\\ \\=\\ https://${localconfig::http_name}:8443",
             ],
             'trusted.secret' => $localconfig::serverprotectsec,
         }
     }
 
-    # Solr Client
+    # QoS filter rate-limits the app server so it won't fall over
+    oae::app::server::sling_config {
+        "org.sakaiproject.nakamura.http.qos.QoSFilter":
+        config => { 'qos.default.limit' => 50, }
+    }
+
+    ###########################################################################
+    # Search
+
+    # Specify the client type
+    oae::app::server::sling_config {
+        "org.sakaiproject.nakamura.solr.SolrServerServiceImpl":
+        config => { "solr-impl" => "multiremote", }
+    }
+    # Configure the client with the master/slave(s) info
     oae::app::server::sling_config {
         "org.sakaiproject.nakamura.solr.MultiMasterRemoteSolrClient":
         config => {
@@ -156,50 +180,41 @@ node oaeappnode inherits oaenode {
         }
     }
 
-    # Specify the client type
-    oae::app::server::sling_config {
-        "org.sakaiproject.nakamura.solr.SolrServerServiceImpl":
-        config => { "solr-impl" => "multiremote", }
-    }
-
+    ###########################################################################
     # Clustering
-    # Note that IP address will be evaluated ON the node. Not in this nodetype
+
+    # Note that IP address will be evaluated by puppet on the node.
     oae::app::server::sling_config {
         "org.sakaiproject.nakamura.cluster.ClusterTrackingServiceImpl":
         config => { 'secure-host-url' => "http://${ipaddress}:8081", }
     }
 
-    # Clustered Cache
+    # Clustered cache
     oae::app::server::sling_config {
         "org.sakaiproject.nakamura.memory.CacheManagerServiceImpl":
         config => { 'bind-address' => $ipaddress, }
     }
 
-    # CLE integration
-    oae::app::server::sling_config {
-        "org.sakaiproject.nakamura.basiclti.CLEVirtualToolDataProvider":
-        config => {
-            'sakai.cle.basiclti.secret' => "secret",
-            'sakai.cle.server.url'      => "https://${http_name}",
-            'sakai.cle.basiclti.key'    => "12345",
-        }
-    }
-
-    # QoS filter
-    oae::app::server::sling_config {
-        "org.sakaiproject.nakamura.http.qos.QoSFilter":
-        config => { 'qos.default.limit' => 50, }
-    }
-
     # Keep an eye on caching in staging
     oae::app::server::sling_config {
         'org.apache.sling.commons.log.LogManager.factory.config-caching':
-	locked => false,
+        locked => false,
         config => {
             'org.apache.sling.commons.log.names' => ["org.sakaiproject.nakamura.memory","net.sf.ehcache"],
             'org.apache.sling.commons.log.level' => "trace",
             'org.apache.sling.commons.log.file'  => "logs/cache.log",
             'service.factoryPid'                 => "org.apache.sling.commons.log.LogManager.factory.config",
+        }
+    }
+
+    ###########################################################################
+    # CLE integration
+    oae::app::server::sling_config {
+        "org.sakaiproject.nakamura.basiclti.CLEVirtualToolDataProvider":
+        config => {
+            'sakai.cle.basiclti.secret' => "secret",
+            'sakai.cle.server.url'      => "https://${localconfig::http_name}",
+            'sakai.cle.basiclti.key'    => "12345",
         }
     }
 }
@@ -225,31 +240,31 @@ node 'staging-app2.academic.rsmart.local' inherits oaeappnode {
 #
 # OAE Solr Nodes
 #
-
 node solrnode inherits oaenode {
+    # All of the solr servers get tomcat
     class { 'tomcat6':
-            parentdir => "${localconfig::basedir}",
-            tomcat_user           => $localconfig::user,
-            tomcat_group          => $localconfig::group,
-            admin_user            => $localconfig::tomcat_user,
-            admin_password        => $localconfig::tomcat_password,
+        parentdir      => $localconfig::basedir,
+        tomcat_user    => $localconfig::user,
+        tomcat_group   => $localconfig::group,
+        admin_user     => $localconfig::tomcat_user,
+        admin_password => $localconfig::tomcat_password,
     }
 }
 
 node 'staging-solr1.academic.rsmart.local' inherits solrnode {
     class { 'oae::solr::tomcat':
-        master_url => "${localconfig::solr_remoteurl}/replication",
-        solrconfig => 'localconfig/master-solrconfig.xml.erb',
-        tomcat_user => $localconfig::user,
+        master_url   => "${localconfig::solr_remoteurl}/replication",
+        solrconfig   => 'localconfig/master-solrconfig.xml.erb',
+        tomcat_user  => $localconfig::user,
         tomcat_group => $localconfig::group,
     }
 }
 
 node /staging-solr[2-3].academic.rsmart.local/ inherits solrnode {
     class { 'oae::solr::tomcat':
-        master_url => "${localconfig::solr_remoteurl}/replication",
-        solrconfig => 'localconfig/master-solrconfig.xml.erb',
-        tomcat_user => $localconfig::user,
+        master_url   => "${localconfig::solr_remoteurl}/replication",
+        solrconfig   => 'localconfig/slave-solrconfig.xml.erb',
+        tomcat_user  => $localconfig::user,
         tomcat_group => $localconfig::group,
     }
 }

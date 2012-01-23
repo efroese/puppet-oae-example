550344a034ca1f339da8c99d3cd1fdd26c56042d
Erik Froese <erik.froese@gmail.com>
2012-01-23 15:11:38 -0500

    Fix the grep so a trailing space doesnt mess up the check.

--- manifests/clientauth.pp
+++ manifests/clientauth.pp
@@ -7,8 +7,14 @@ define postgres::clientauth (
     $option=""
     ) {
 
-    $config_line = "${type} ${db} ${user} ${address} ${method} ${option}"
-    $hba_conf    = '/var/lib/pgsql/data/pg_hba.conf'
+    if $option == "" {
+        $config_line = "${type} ${db} ${user} ${address} ${method}",
+    }
+    else {
+        $config_line = "${type} ${db} ${user} ${address} ${method} ${option}"
+    }
+
+    $hba_conf = '/var/lib/pgsql/data/pg_hba.conf'
 
     exec { "append-${config_line}-to-${hba_conf}":
         command => "echo ${config_line} >> ${hba_conf}",

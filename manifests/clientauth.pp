e7a0d3fb2fd7c282b234631a417ca51c91378579
Erik Froese <erik.froese@gmail.com>
2012-01-23 15:01:28 -0500

    Fix grep check. Notify postgresql.

--- manifests/clientauth.pp
+++ manifests/clientauth.pp
@@ -10,8 +10,9 @@ define postgres::clientauth (
     $config_line = "${type} ${db} ${user} ${address} ${method} ${option}"
     $hba_conf    = '/var/lib/pgsql/data/pg_hba.conf'
 
-    exec { "Append host line to pg_hba.conf":
+    exec { "append-${config_line}-to-${hba_conf}":
         command => "echo ${config_line} >> ${hba_conf}",
-        unless  => "grep ${config_line} ${hba_conf}"
+        unless  => "grep '${config_line}' ${hba_conf}",
+        notify  => Service['postgresql'],
     }
 }

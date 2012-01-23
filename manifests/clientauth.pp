a13721e33072aa6209c0d0d7e8fbb49e00b045de
Erik Froese <erik.froese@gmail.com>
2012-01-23 14:30:03 -0500

    The name param is just a resource id now. Use the db param.

--- manifests/clientauth.pp
+++ manifests/clientauth.pp
@@ -1,12 +1,13 @@
 define postgres::clientauth (
     $type,
+    $db,
     $user,
     $address,
     $method,
     $option,
     ) {
 
-    $config_line = "${type} ${name} ${user} ${address} ${method} ${option}"
+    $config_line = "${type} ${db} ${user} ${address} ${method} ${option}"
     $hba_conf    = '/var/lib/pgsql/data/pg_hba.conf'
 
     exec { "Append host line to pg_hba.conf":

5a13b402442b0e1bab693f923832f1595cada3f0
Erik Froese <erik.froese@gmail.com>
2012-01-26 15:25:17 -0500

    Simplify the client module and support amazon linux.

--- manifests/client/redhat.pp
+++ manifests/client/redhat.pp
@@ -3,50 +3,30 @@ class nfs::client::redhat inherits nfs::base {
   package { "nfs-utils":
     ensure => present,
   }
+  
+  $portmap = "${operatingsystem}-${operatingsystemrelease}" ? {
+      /Amazon-2011.09/ => 'rpcbind',
+      default          => 'portmap',
+  }
 
-  if $lsbmajdistrelease == 6 {
-
-    package {"rpcbind":
-      ensure => present,
-    }
-
-    service {"rpcbind":
-      ensure    => running,
-      enable    => true,
-      hasstatus => true,
-      require   => [Package["rpcbind"], Package["nfs-utils"]],
-    }
-
-  } else {
-
-    package { "portmap":
-      ensure => present,
-    }    
-    
-    service { "portmap":
-      ensure    => running,
-      enable    => true,
-      hasstatus => true,
-      require   => [Package["portmap"], Package["nfs-utils"]],
-    }
-
+  package { $portmap: ensure => installed }
+  service { $portmap:
+      ensure => running,
+      enable => true,
+      require => [ Package[$portmap], Package["nfs-utils"]],
   }
 
   service {"nfslock":
     ensure    => running,
     enable    => true,
     hasstatus => true,
-    require   => $lsbmajdistrelease ? {
-      6       => Service["rpcbind"],
-      default => [Package["portmap"], Package["nfs-utils"]]
-    },
+    require   => [Package[$portmap], Package["nfs-utils"]],
   }
  
   service { "netfs":
     enable  => true,
     require => $lsbmajdistrelease ? {
-      6       => Service["nfslock"],
-      default => [Service["portmap"], Service["nfslock"]],
+      default => [Service[$portmap], Service["nfslock"]],
     },
   }
 
--- manifests/server/redhat.pp
+++ manifests/server/redhat.pp
@@ -1,17 +1,5 @@
 class nfs::server::redhat inherits nfs::client::redhat {
 
-    $portmap = "${operatingsystem}-${operatingsystemrelease}" ? {
-        /Amazon-2011.09/ => 'rpcbind',
-        default          => 'portmap',
-    }
-
-    package { $portmap: ensure => installed }
-    service { $portmap:
-        ensure => running,
-        enabled => true,
-        require => Package[$portmap]
-    }
-
     exec { 'reload_nfs_srv':
         command     => "/etc/init.d/nfs reload",
         refreshonly => true,
@@ -21,6 +9,6 @@ class nfs::server::redhat inherits nfs::client::redhat {
     service {'nfs':
         enable  => "true",
         ensure  => "running",
-        require => [ Package["nfs-utils"], Service[$portmap], ]
+        require => Package["nfs-utils"],
     }
 }

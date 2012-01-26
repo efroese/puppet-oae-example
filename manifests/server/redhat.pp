8174c9c764dba70dbc8a5a08e00af7b6872be348
Erik Froese <erik.froese@gmail.com>
2012-01-26 15:05:06 -0500

    Add support for Amazon AMI 2011.09 as an nfs server.

--- manifests/server/redhat.pp
+++ manifests/server/redhat.pp
@@ -1,5 +1,17 @@
 class nfs::server::redhat inherits nfs::client::redhat {
-  
+
+    $portmap = "${operatingsystem}-${operatingsystemrelease}":
+        /Amazon-2011.09/ => 'rpcbind',
+        default          => 'portmap',
+    }
+
+    package { $portmap: ensure => installed }
+    service { $portmap:
+        ensure => running,
+        enabled => true,
+        require => Package[$portmap]
+    }
+
     exec { 'reload_nfs_srv':
         command     => "/etc/init.d/nfs reload",
         refreshonly => true,
@@ -8,6 +20,7 @@ class nfs::server::redhat inherits nfs::client::redhat {
 
     service {'nfs':
         enable  => "true",
-        ensure  => "running"
+        ensure  => "running",
+        require => [ Package["nfs-utils"], Service[$portmap], ]
     }
 }

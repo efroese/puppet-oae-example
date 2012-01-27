d0ee8800153ef7f01bd4e7106216449b6685189e
Erik Froese <erik.froese@gmail.com>
2012-01-27 14:13:02 -0500

    Add the name attribute to the yum repo.

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -11,6 +11,7 @@ class centos {
 
     if $operatingsystem == 'Amazon' and $operatingsystemrelease == '2011.09' {
         yumrepo { 'centos6-base':
+            name       => 'centos6-base',
             mirrorlist => "http://mirrorlist.centos.org/?release=6&arch=${architecture}&repo=os",
             gpgcheck   => '1',
             gpgkey     => 'http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6',

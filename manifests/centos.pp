abc8b5080d60590d2d23e414959f4e8746d99d1f
Erik Froese <erik.froese@gmail.com>
2012-01-27 12:36:08 -0500

    Add rpmforge yum repos. Add centos class to Amazon servers.

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,4 +1,10 @@
 class centos {
 	package { 'redhat-lsb': ensure => installed }
-	package { 'pwgen': ensure => installed }
+
+    if $lsbmajdistrelease == '6' or
+        ($operatingsystem == 'Amazon' and $operatingsystemrelease == '2011.09') {
+	    package { 'http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm':
+	        ensure => installed
+        }
+	}
 }

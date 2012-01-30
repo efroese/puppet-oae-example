9b846a151a57763c0e57f4d90cba8d6d40e94ec0
Erik Froese <erik.froese@gmail.com>
2012-01-30 10:28:20 -0500

    Add the nakamura, rbtagger, and term-extract gems.

--- manifests/preview_processor/gems.pp
+++ manifests/preview_processor/gems.pp
@@ -26,12 +26,15 @@ class oae::preview_processor::gems {
         }
     }
 
+    $nakamura_gem_version = '0.6'
     $curb_gem_version     = '0.7.15'
     $docsplit_gem_version = '0.6.3'
     $json_gem_version     = '1.6.3'
     $rmagick_gem_version  = '2.13.1'
     $getopt_gem_version   = '1.4.1'
     $daemons_gem_version  = '1.1.5'
+    $rbtagger_version     = '0.4.7'
+    $termextract_gem_version  = '0.5.2'
 
     if $operatingsystem == 'CentOS' and $lsbmajdistrelease == '5' {
         opt_gem { 'curb':     version => $curb_gem_version }
@@ -40,14 +43,8 @@ class oae::preview_processor::gems {
         opt_gem { 'rmagick':  version => $rmagick_gem_version }
         opt_gem { 'getopt':   version => $getopt_gem_version }
         opt_gem { 'daemons':  version => $daemons_gem_version }
-
-        exec { 'patch-docsplit':
-            command => "patch -p0 < ${oae::params::basedir}/patches/info_extractor.rb.patch info_extractor.rb",
-            cwd     => "/opt/local/lib64/ruby/gems/1.9.1/gems/docsplit-${docsplit_gem_version}/lib/docsplit/",
-            require => [File["${oae::params::basedir}/patches/info_extractor.rb.patch"], Opt_gem['docsplit']],
-            unless  => "grep Iconv /opt/local/lib64/ruby/gems/1.9.1/gems/docsplit-${docsplit_gem_version}/lib/docsplit/info_extractor.rb"
-        }
-
+        opt_gem { 'rbtagger': version => $rbtagger_version }
+        opt_gem { 'term-extract': version => $termextract_gem_version }
     } 
     else {
         gem { 'curb':     version => $curb_gem_version }
@@ -56,13 +53,23 @@ class oae::preview_processor::gems {
         gem { 'rmagick':  version => $rmagick_gem_version }
         gem { 'getopt':   version => $getopt_gem_version }
         gem { 'daemons':  version => $daemons_gem_version }
-        
-        exec { 'patch-docsplit': 
-            command => "patch -p0 < ${oae::params::basedir}/patches/info_extractor.rb.patch info_extractor.rb",
-            cwd     => "/usr/lib/ruby/gems/1.8/gems/docsplit-${docsplit_gem_version}/lib/docsplit",
-            require => [File["${oae::params::basedir}/patches/info_extractor.rb.patch"], Gem['docsplit']],
-            unless  => "grep Iconv /usr/lib/ruby/gems/1.8/gems/docsplit-${docsplit_gem_version}/lib/docsplit/info_extractor.rb"
-        }
+        gem { 'rbtagger': version => $rbtagger_version }
+        gem { 'term-extract': version => $termextract_gem_version }
+    }
+
+    $docsplit_gem_path = $lsbmajdistrelease ? {
+        '5'     => "/opt/local/lib64/ruby/gems/1.9.1/gems/docsplit-${docsplit_gem_version}",
+        default => "/usr/lib/ruby/gems/1.8/gems/docsplit-${docsplit_gem_version}",
+    }
+
+    exec { "patch-docsplit":
+        command => "patch -p0 < ${oae::params::basedir}/patches/info_extractor.rb.patch info_extractor.rb",
+        cwd     => "${docsplit_gem_path}/lib/docsplit/",
+        unless  => "grep Iconv ${docsplit_gem_path}/lib/docsplit/info_extractor.rb",
+        require => $lsbmajdistrelease ? {
+            5       => [ File["${oae::params::basedir}/patches/info_extractor.rb.patch"], Opt_gem['docsplit'] ],
+            default => [ File["${oae::params::basedir}/patches/info_extractor.rb.patch"], gem['docsplit'] ],
+        },
     }
 
     file { "${oae::params::basedir}/patches":

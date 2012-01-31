3c20e7d14442b729a2ec3dfad4ba104f3bd8018f
Erik Froese <erik.froese@gmail.com>
2012-01-31 09:54:49 -0500

    Specify the url to upload to.

--- nodes-rsmart-cluster.pp
+++ nodes-rsmart-cluster.pp
@@ -280,6 +280,7 @@ node /solr[2-3].academic.rsmart.local/ inherits solrnode {
 #
 node 'preview.academic.rsmart.local' inherits oaenode {
     class { 'oae::preview_processor::init':
+        upload_url     => "https://{$localconfig::http_name}/"
         admin_password => $localconfig::admin_password,
         nakamura_git => $localconfig::nakamura_git,
         nakamura_tag => $localconfig::nakamura_tag,

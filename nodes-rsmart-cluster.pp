ee6b0882ae7de3f22879a9468c6b292faec40c6c
Lance Speelmon <lance@speelmon.com>
2012-02-04 00:49:06 -0700

    NOJIRA troubleshooting no live solr servers error, bump up values

--- nodes-rsmart-cluster.pp
+++ nodes-rsmart-cluster.pp
@@ -175,6 +175,10 @@ node oaeappnode inherits oaenode {
         config => {
             "remoteurl"  => $localconfig::solr_remoteurl,
             "query-urls" => $localconfig::solr_queryurls,
+            "query-so-timeout" => 10000,
+            "query-connection-timeout" => 1000,
+            "query-connection-manager-timeout" => 1000,
+            "connection.timeout" => 1000,
         }
     }
 

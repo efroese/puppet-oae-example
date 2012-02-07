e481dac98c05d5881eafd72fb4664975274e7e61
Erik Froese <erik.froese@gmail.com>
2012-02-07 15:17:51 -0500

    Stupid friggin AMI's and their operatingsystems.

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,7 +1,7 @@
 class nfs::client {
   case $operatingsystem {
     Debian,Ubuntu:        { include nfs::client::debian}
-    RedHat,CentOS,Amazon: { include nfs::client::redhat}
+    RedHat,CentOS,Amazon,Linux: { include nfs::client::redhat}
     default:              { fail "Unsupported operatingsystem ${operatingsystem}" }
   }
 }

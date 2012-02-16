6d4a762f659d4268a963bc011b9983eb19f00075
Lance Speelmon <lance@speelmon.com>
2012-02-16 16:10:10 -0700

    NOJIRA disable mock cle content

--- modules/rsmart-oipp-prod-config/manifests/init.pp
+++ modules/rsmart-oipp-prod-config/manifests/init.pp
@@ -57,7 +57,7 @@ class localconfig {
     $apache_lb_members_untrusted = [ "${app_server1}:8082", "${app_server2}:8082" ]
     $apache_lb_params            = ["retry=20", "min=3", "flushpackets=auto", "max=1", "loadfactor=100", "timeout=300"]
 
-    $mock_cle_content = true
+    $mock_cle_content = false
     $apache_cle_lb_members = [ "${cle_server1}:8009 route=OIPP-CLE1", "${cle_server2}:8009 route=OIPP-CLE2" ]
     $apache_cle_location_match = "^/(xsl-portal.*|access.*|courier.*|dav.*|direct.*|imsblti.*|library.*|messageforums-tool.*|osp-common-tool.*|polls-tool.*|portal.*|profile-tool.*|profile2-tool.*|sakai.*|samigo-app.*|scheduler-tool.*|rsmart-customizer-tool.*|oauth-tool.*|emailtemplateservice-tool.*|sitestats-tool.*|rsmart-support-tool.*|mailsender-tool.*|tool.css|portool_base.css)" 
     $cle_dav_server0 = '10.52.10.19'

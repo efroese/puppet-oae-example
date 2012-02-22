b5bcbc788e579cdb7da616a446caebab04fc61f6
Erik Froese <erik.froese@gmail.com>
2012-02-21 21:54:37 -0500

    Add all of the openoffice.org-*-core packages.

--- manifests/preview_processor/openoffice.pp
+++ manifests/preview_processor/openoffice.pp
@@ -5,7 +5,7 @@
 class oae::preview_processor::openoffice {
 
     if $operatingsystem =~ /Amazon|Linux/ {
-        $pkg_string = 'openoffice.org-core.x86_64 openoffice.org-javafilter.x86_64 openoffice.org-headless.x86_64 openoffice.org-writer.x86_64'
+        $pkg_string = 'openoffice.org-core.x86_64 openoffice.org-javafilter.x86_64 openoffice.org-headless.x86_64 openoffice.org-base-core.x86_64 openoffice.org-calc-core.x86_64 openoffice.org-draw-core.x86_64 openoffice.org-math-core.x86_64 openoffice.org-writer-core.x86_64'
         exec { 'install-ooo-centos':
             command => "yum -y --enablerepo=centos6-base install ${pkg_string}",
             unless  => "rpm -q ${pkg_string}",
@@ -13,8 +13,14 @@ class oae::preview_processor::openoffice {
         }
     }
     else {
-        package { [ 'openoffice.org-core', 'openoffice.org-javafilter',
-                    'openoffice.org-headless',  'openoffice.org-writer' ]: 
+        package { [ 'openoffice.org-core',
+                    'openoffice.org-javafilter',
+                    'openoffice.org-headless',
+                    'openoffice.org-base-core',
+                    'openoffice.org-calc-core',
+                    'openoffice.org-draw-core',
+                    'openoffice.org-math-core',
+                    'openoffice.org-writer-core' ]:
             ensure => installed,
             notify => [ Service['soffice'], File['/usr/lib/openoffice'] ],
         }

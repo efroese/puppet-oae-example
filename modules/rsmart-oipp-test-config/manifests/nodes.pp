01fb8e7a3dd565e0d1117296feed6fafe765fdb9
Erik Froese <erik.froese@gmail.com>
2012-02-27 15:17:12 -0500

    Fix the use of the tomcat6::overlay

--- modules/rsmart-oipp-test-config/manifests/nodes.pp
+++ modules/rsmart-oipp-test-config/manifests/nodes.pp
@@ -282,11 +282,12 @@ node /oipp-test[2]?.academic.rsmart.local/ inherits oaenode {
 	    setenv_template      => 'localconfig/cle-setenv.sh.erb',
     }
 
-    tomcat::overlay { 'rsmart-cle-prod-overlay':
+    tomcat6::overlay { 'rsmart-cle-prod-overlay':
         tomcat_home  => "${localconfig::homedir}/sakaicle/tomcat",
         overlay_path => "${localconfig::homedir}/sakaicle/rsmart-cle-prod-overlay.tbz",
         creates      => 'webapps/ROOT/rsmart.jsp',
         user         => $oae::params::user,
+        require      => Class['Tomcat6']
     }
 
     class { 'cle':

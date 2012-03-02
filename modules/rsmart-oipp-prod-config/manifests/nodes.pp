39dfd2494c62ceed8e67bf3bf34fdf8b6d85f550
Erik Froese <erik.froese@gmail.com>
2012-03-02 12:33:43 -0500

    ACAD-793 fix template names.

--- modules/rsmart-oipp-prod-config/manifests/nodes.pp
+++ modules/rsmart-oipp-prod-config/manifests/nodes.pp
@@ -150,9 +150,9 @@ node 'apache1.academic.rsmart.local' inherits oaenode {
 
     class { 'sendmail':
         sendmail_mc_template      => 'localconfig/sendmail.mc.erb',
-        access_template           => 'localconfig/access.mc.erb',
-        mailertable_template      => 'localconfig/mailertable.mc.erb',
-        virtusertable_template    => 'localconfig/virtusertable.mc.erb',
+        access_template           => 'localconfig/access.erb',
+        mailertable_template      => 'localconfig/mailertable.mc',
+        virtusertable_template    => 'localconfig/virtusertable.erb',
         local_host_names_template => 'localconfig/local_host_names.erb'
     }
     ###########################################################################

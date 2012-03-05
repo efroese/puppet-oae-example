6b807c7164ac7dc25920f14fd203c137e7a81033
Erik Froese <erik.froese@gmail.com>
2012-03-05 14:09:35 -0500

    Install openoffice.org-brand on rhel6/amazon.

--- manifests/preview_processor/openoffice.pp
+++ manifests/preview_processor/openoffice.pp
@@ -5,7 +5,7 @@
 class oae::preview_processor::openoffice {
 
     if $operatingsystem =~ /Amazon|Linux/ {
-        $pkg_string = 'openoffice.org-core.x86_64 openoffice.org-javafilter.x86_64 openoffice.org-headless.x86_64 openoffice.org-base-core.x86_64 openoffice.org-calc-core.x86_64 openoffice.org-draw-core.x86_64 openoffice.org-math-core.x86_64 openoffice.org-writer-core.x86_64'
+        $pkg_string = 'openoffice.org-core.x86_64 openoffice.org-javafilter.x86_64 openoffice.org-headless.x86_64 openoffice.org-base-core.x86_64 openoffice.org-calc-core.x86_64 openoffice.org-draw-core.x86_64 openoffice.org-math-core.x86_64 openoffice.org-writer-core.x86_64 openoffice.org-brand.x86_64'
         exec { 'install-ooo-centos':
             command => "yum -y --enablerepo=centos6-base install ${pkg_string}",
             unless  => "rpm -q ${pkg_string}",
@@ -14,6 +14,7 @@ class oae::preview_processor::openoffice {
     }
     else {
         package { [ 'openoffice.org-core',
+                    'openoffice.org-brand',
                     'openoffice.org-javafilter',
                     'openoffice.org-headless',
                     'openoffice.org-base-core',

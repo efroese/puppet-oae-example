b08d94cbdf1d1d2d2369f06e1d382efbaf6795ac
Erik Froese <erik.froese@gmail.com>
2012-03-06 15:35:07 -0500

    Use puppet environments.

--- bin/apply.sh
+++ bin/apply.sh
@@ -2,10 +2,30 @@
 #
 # apply.sh - Erik Froese <erik@hallwaytech.com>
 #
-# Run puppet with the correct module path.
+# This is a convenience script to call puppet on a node with no puppet master.
+# The puppet-oae-example project is intended to work in an environment with no
+# puppetmaster server. It hasn't been tested with a puppet master.
+# 
+# In order to enable a specific configuration use the --environment parameter.
+# Available environments are listed in the puppet-oae-example/environments/ folder.
+#
+# If you want to automatically specify the environment put the name in
+# puppet-oae-example/.environment
 #
 # For more info pass the --debug or --verbose flags.
 #
-export RUBYOPT=rubygems
-PUPPET=/usr/lib/ruby/gems/1.8/gems/puppet-2.7.9/bin/puppet
-$PUPPET apply --modulepath modules site.pp $@
+
+ENV_FILE=`pwd`/.environment
+ENV_ARG=""
+
+echo $@ | grep -q environment
+if [[ $? -eq 1 && -f $ENV_FILE ]]; then
+    echo "loaded environment from ${ENV_FILE}"
+    echo "environment = `cat ${ENV_FILE}`"
+    ENV_ARG="--environment `cat ${ENV_FILE}`"
+fi
+
+# Look up modules in:
+# 1. environments/$environment/modules/
+# 2. modules/
+puppet apply --modulepath environments/\$environment/modules:modules site.pp $ENV_ARG $@

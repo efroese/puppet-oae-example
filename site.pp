0869d0811b3bc0557631e5c39edfadf7fdabba2f
Erik Froese <erik.froese@gmail.com>
2012-03-06 15:35:07 -0500

    Use puppet environments.

--- site.pp
+++ site.pp
@@ -5,7 +5,7 @@ import 'nodetypes'
 # Dynamically import the nodes list based on the active configuration.
 # Activate the correct configuration by making a symlink in the modules directory to the right config module.
 # 
-import 'modules/localconfig/manifests/nodes.pp'
+import 'localconfig/manifests/nodes.pp'
 
 # Set the default path for exec resources
 Exec { path => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin' }

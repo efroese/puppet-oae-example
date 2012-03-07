4393ae49e9cd4777a5f6c6231a3bedc2d469790f
Duffy Gillman <duffy@rsmart.com>
2012-03-07 15:08:40 -0700

    ACAD-811 added oipp-test to /etc/hosts for OIPP prod apache

--- modules/oipp/manifests/init.pp
+++ modules/oipp/manifests/init.pp
@@ -7,6 +7,12 @@ class oipp::sis {
     class { 'people::oipp-sis::internal': }
     class { 'people::oipp-sis::external': }
 
+    host { 'oipp-test':
+	ensure => present,
+	ip => 10.51.9.112,
+	alias=> 'oipp-test'.
+    }
+
     file { "/root/scripts/oipp_csv_copy.sh":
         owner => root,
         group => root,

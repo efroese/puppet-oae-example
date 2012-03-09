d109726944efce0cc0ea1a89772bcaf9b077b6e5
Erik Froese <erik.froese@gmail.com>
2012-03-09 15:02:34 -0500

    NOJIRA - More configurableness.

--- modules/sis/manifests/batch.pp
+++ modules/sis/manifests/batch.pp
@@ -13,25 +13,47 @@
 class sis::batch (
     $user,
     $executable_url,
-    $artifact
+    $artifact,
+    $sis_properties = false
     ) inherits sis {
+
+    file { "${sis::basedir}/batch":
+        ensure => directory,
+    }
+
+    $home = "${sis::basedir}/batch"
 
     # download the jar to $HOME/sis/artifact.jar
     archive::download { $artifact:
         ensure        => present,
         url           => $executable_url,
-        src_target    => $sis::basedir,
+        src_target    => $home,
         checksum      => false,
         timeout       => 0,
+        require => File[$sis::batch::home],
     }
 
-    file { "${sis::basedir}/bin/run_sis_batch.sh":
+    file { "${sis::batch::home}/bin":
+        ensure => directory,
+        require => File[$sis::batch::home],
+    }
+
+    if $sis_properties {
+        file { "${sis::batch::home}/sis.properties":
+            mode => 0644,
+            content => template($sis_properties),
+            require => File[$sis::batch::home],
+        }
+    }
+
+    file { "${sis::batch::home}/bin/run_sis_batch.sh":
         mode => 0755,
         content => template('sis/run_sis_batch.sh.erb'),
+        require => File["${sis::batch::home}/bin"],
     }
 
     cron { 'rsmart-basic-sis-batch':
-        command => "${sis::basedir}/bin/run_sis_batch.sh",
+        command => "${sis::batch::home}/bin/run_sis_batch.sh",
         user    => $user,
         minute  => 0,
     }

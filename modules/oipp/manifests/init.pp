1213c6a5a1ef346b93a4e5c637cb0cd59341b706
Duffy Gillman <duffy@rsmart.com>
2012-03-12 14:14:36 -0700

    ACAD-836 used * character to pick up all SIS import files in oipp_csv_copy script

--- modules/oipp/manifests/init.pp
+++ modules/oipp/manifests/init.pp
@@ -8,9 +8,9 @@ class oipp::sis {
     class { 'people::oipp-sis::external': }
 
     host { 'oipp-test':
-	ensure => present,
-	ip => '10.51.9.112',
-	alias=> 'oipp-test',
+	    ensure => present,
+	    ip => '10.51.9.112',
+	    alias=> 'oipp-test',
     }
 
     file { "/root/scripts/oipp_csv_copy.sh":
@@ -34,5 +34,23 @@ class oipp::sis {
 
 class oipp::test {
 
+    class { 'people::oipp-sis::external': }
     class { 'people::oipp-sis::destination': }
+
+    file { "/root/scripts/oipp_csv_copy.sh":
+        owner => root,
+        group => root,
+        mode => 0750,
+        content => template('oipp/oipp_csv_copy_test.sh.erb'),
+    }
+
+    cron { 'transport_sis':
+        command => "/root/scripts/oipp_csv_copy.sh",
+        user => root,
+        ensure => present,
+        minute => '2',
+        require => [
+            File["/root/scripts/oipp_csv_copy.sh"],
+        ],
+    }
 }

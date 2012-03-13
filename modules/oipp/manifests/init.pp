1366383bc6e392a8fb64605ae575180e57cb881d
Duffy Gillman <duffy@rsmart.com>
2012-03-13 10:52:49 -0700

    ACAD-836 added oipp-prod-apache1's root pub keys to the sis users on oipp-test to emulate file xfers

--- modules/oipp/manifests/init.pp
+++ modules/oipp/manifests/init.pp
@@ -69,4 +69,27 @@ class oipp::test {
             File["/root/scripts/oipp_csv_copy.sh"],
         ],
     }
+
+    # add root's key from oipp-apache1 to OIPP users on test to emulate file xfer
+    ssh_authorized_key { 'ucb_sis-test-pub':
+        ensure => present,
+        key  => 'AAAAB3NzaC1yc2EAAAABIwAAAgEAr1DlzFukqnTX7AxDOmej1InLft96X65KdVrU7EPGBKsfA4RYH9b7NYzmgg9lqHNcixoyLS6mgdxt0j7ZX2YzY0CgUeuAo/bNOtn8HNAc0qvFnx4qeR+DFK/9MPBDECy2qOqDJFNvaAWilAewtAXMtpORJRtonzpJwRyDAmW8umnqcvLlORcX4ls3cUc0O8Nxh1YrrCqkeZKfI6Wj//3Ve5R3QPsgX+2GRkvh+7I3XuGE72iYdnVXDPWRIT5Hc0Cf/Effw/Xyr14ujRRr07rGawP6YrMgPzOTf9OPaRCshCcXH79a95qxiaVP5P9Op0ZJjHAtE+E6y5FB2yqeyfCoXMFOzUfnmSXZW81iNg/rGeF1zmkcZIkEOb3RNukg5LWg47UYEFFeqwW+zWNfpOo6aJmABFvS5yg/vJxV5HeyCAcNS+pS+zBVK9oRakMXoleTSr/dPi07JThN3DghxJ5R9e4qbX2oYkE6OLHQpp+F76plz5+Kidt5wJOQHsVO6A3mWwg988ySbqRTAnaIeeSjjTpG1Bu8AkFbcAgvxVhKzm89BbHr9NWtAxo+zWFp/6dLL44QCV0juxY1nQC9RDqTkfMVxjF5TsN8sXBfTMO7irrW9hH9GL/Y+EzhspEUpPqBCBBhEnc+QDhr4NMmZjXign8ef9SXohNvtfOkRLCl4E0=',
+        type => 'ssh-rsa',
+        user => 'ucb_sis',
+        require => User['ucb_sis'],
+    }
+    ssh_authorized_key { 'ucd_sis-test-pub':
+        ensure => present,
+        key  => 'AAAAB3NzaC1yc2EAAAABIwAAAgEAr1DlzFukqnTX7AxDOmej1InLft96X65KdVrU7EPGBKsfA4RYH9b7NYzmgg9lqHNcixoyLS6mgdxt0j7ZX2YzY0CgUeuAo/bNOtn8HNAc0qvFnx4qeR+DFK/9MPBDECy2qOqDJFNvaAWilAewtAXMtpORJRtonzpJwRyDAmW8umnqcvLlORcX4ls3cUc0O8Nxh1YrrCqkeZKfI6Wj//3Ve5R3QPsgX+2GRkvh+7I3XuGE72iYdnVXDPWRIT5Hc0Cf/Effw/Xyr14ujRRr07rGawP6YrMgPzOTf9OPaRCshCcXH79a95qxiaVP5P9Op0ZJjHAtE+E6y5FB2yqeyfCoXMFOzUfnmSXZW81iNg/rGeF1zmkcZIkEOb3RNukg5LWg47UYEFFeqwW+zWNfpOo6aJmABFvS5yg/vJxV5HeyCAcNS+pS+zBVK9oRakMXoleTSr/dPi07JThN3DghxJ5R9e4qbX2oYkE6OLHQpp+F76plz5+Kidt5wJOQHsVO6A3mWwg988ySbqRTAnaIeeSjjTpG1Bu8AkFbcAgvxVhKzm89BbHr9NWtAxo+zWFp/6dLL44QCV0juxY1nQC9RDqTkfMVxjF5TsN8sXBfTMO7irrW9hH9GL/Y+EzhspEUpPqBCBBhEnc+QDhr4NMmZjXign8ef9SXohNvtfOkRLCl4E0=',
+        type => 'ssh-rsa',
+        user => 'ucd_sis',
+        require => User['ucd_sis'],
+    }
+    ssh_authorized_key { 'ucm_sis-test-pub':
+        ensure => present,
+        key  => 'AAAAB3NzaC1yc2EAAAABIwAAAgEAr1DlzFukqnTX7AxDOmej1InLft96X65KdVrU7EPGBKsfA4RYH9b7NYzmgg9lqHNcixoyLS6mgdxt0j7ZX2YzY0CgUeuAo/bNOtn8HNAc0qvFnx4qeR+DFK/9MPBDECy2qOqDJFNvaAWilAewtAXMtpORJRtonzpJwRyDAmW8umnqcvLlORcX4ls3cUc0O8Nxh1YrrCqkeZKfI6Wj//3Ve5R3QPsgX+2GRkvh+7I3XuGE72iYdnVXDPWRIT5Hc0Cf/Effw/Xyr14ujRRr07rGawP6YrMgPzOTf9OPaRCshCcXH79a95qxiaVP5P9Op0ZJjHAtE+E6y5FB2yqeyfCoXMFOzUfnmSXZW81iNg/rGeF1zmkcZIkEOb3RNukg5LWg47UYEFFeqwW+zWNfpOo6aJmABFvS5yg/vJxV5HeyCAcNS+pS+zBVK9oRakMXoleTSr/dPi07JThN3DghxJ5R9e4qbX2oYkE6OLHQpp+F76plz5+Kidt5wJOQHsVO6A3mWwg988ySbqRTAnaIeeSjjTpG1Bu8AkFbcAgvxVhKzm89BbHr9NWtAxo+zWFp/6dLL44QCV0juxY1nQC9RDqTkfMVxjF5TsN8sXBfTMO7irrW9hH9GL/Y+EzhspEUpPqBCBBhEnc+QDhr4NMmZjXign8ef9SXohNvtfOkRLCl4E0=',
+        type => 'ssh-rsa',
+        user => 'ucm_sis',
+        require => User['ucm_sis'],
+    }
 }

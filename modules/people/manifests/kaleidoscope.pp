f52f2e2b4e6670cf48cdd1e621295f41776c2a8d
Lance Speelmon <lance@speelmon.com>
2012-03-16 11:50:04 -0700

    ACAD-722 Another "real" pubkey from KAL *sigh*

--- modules/people/manifests/kaleidoscope.pp
+++ modules/people/manifests/kaleidoscope.pp
@@ -16,6 +16,7 @@ class people::kaleidoscope::external {
     realize(User['kaleidoscope'])
     realize(Ssh_authorized_key['kaleidoscope-pub'])
     realize(Ssh_authorized_key['kaleidoscope-pub-real'])
+    realize(Ssh_authorized_key['kaleidoscope-pub-real2'])
 }
 
 class people::kaleidoscope {
@@ -58,8 +59,15 @@ class people::kaleidoscope {
         require => User['kaleidoscope'],
     }
     @ssh_authorized_key { 'kaleidoscope-pub-real':
-        ensure => present,
+        ensure => absent,
         key  => 'AAAAB3NzaC1kc3MAAACBAMBGcIYKqrR/W0naV4zw8SMX4Pe0FyGVw5O+E+WqileyI7KYOeO9bDvZiMi/zXL7s9c5tIoZIROZejL4+/uBCRcXVv/bf+w5XlnIRocRLpTCx3YkbZYL9eJB2aGAiEa/9bI1djFc99P9Li1JYx5v9Se9eNj+o1uayV9xtgNPWukTAAAAFQCpz7X2h7CUdvDLtxxpqn5vaYUYswAAAIB8NOhOrV4eKY/Xq3E9jYPydFumdFm4UC1Lc9Aemr0WmfW6tRsowRYNm4UsCg34ZvvUdBXVwOkPtT3MwJnAgVTJ3w85IqK5j4LbSHl4Caxm6ccLN7egeTHU8zw9NfDheeAgR21YhAdWrYzA4mLlwT9iezUAbqDLgPkyVlCVXTVgpAAAAIAnqBaZSw7544zgNQSooOyF4TasHAiOl0txNg/fg7iOm/ltfsCQUzZjE4OApCj+inLp0aL3Nu6rmAhoixxpE/+TS6xxQDSgjA7THaiOnZuipIkO4HMkNY9Z1qapiNULy0bAZje68r3Cx4Jf5j8B8P9djldjwM6VUdmNtIth7Cy0BA==',
+        type => 'ssh-dss',
+        user => 'kaleidoscope',
+        require => User['kaleidoscope'],
+    }
+    @ssh_authorized_key { 'kaleidoscope-pub-real2':
+        ensure => present,
+        key  => 'AAAAB3NzaC1kc3MAAACBAJrlvqcHHmF2xPNxrsINm+RqMe1AF5DTyYUVE0PzAejRBy7/7L8fI1nBkyrQ8Qi6l8gPkna8nhlVF6FmitFf/zEWIH7MAV22mso+ZNJ2ZVvMGGY0VUL82sQyHWL5Pt2HnBoQbKfxqruXvqPfmXQ/eeRfqazF2IVeanytvE/ggPMLAAAAFQCJNqakjbPlUraXIqu8H5Zhcp2HIwAAAIAg8jn5Vt3n4xHNQlrUax6GOtm2IXa3NgbCKx7UhCRwIB7K5pw6vV0LyrIxf+sVrFar0Ob/BURSyh4xE/4N85q2sVJiuBFXaKG612IMgZ+AaBXEGUE88KB2itU66nxy5qW4VVtDjT0GH7WXbpswwbOE1DQ6rhpjmfFVHKd1Eyi3UAAAAIA4oW2teLwPFWAgjIdRv2VvrI85PL7UCfXy0Kp0r4AUfcHPDi7FEmYI4QzzJf9r90lX4e2bDnqcxgiWhFuITP0nOCS4Tqf4zojU5DJEshUXlKJM+3ZphAtaOCZWyfl7l19PXHGFO0MCQtT7hdHjJ23kK8/QhkFFfv96KIfC8lh9Rg==',
         type => 'ssh-dss',
         user => 'kaleidoscope',
         require => User['kaleidoscope'],

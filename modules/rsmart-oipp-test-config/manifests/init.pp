1200e9e25f85464a0308c946573b343ac9b25045
Erik Froese <erik.froese@gmail.com>
2012-03-19 14:26:52 -0400

    NOJIRA Fix the dns servers for oipp-test. Not effective except for suppressing a log error.

--- modules/rsmart-oipp-test-config/manifests/init.pp
+++ modules/rsmart-oipp-test-config/manifests/init.pp
@@ -118,7 +118,7 @@ class localconfig {
 
     $cle_email_test_mode = true
     $cle_smtp_server     = 'localhost'
-    $cle_dns             = [ 'TODO_DNS_1', 'TODO_DNS_2']
+    $cle_dns             = [ '10.51.0.2', '10.51.0.2']
     $cle_mail_support    = 'bogus@mailinator.com'
     $cle_mail_request    = 'bogus@mailinator.com'
     $samigo_stmp_server  = $cle_smtp_server

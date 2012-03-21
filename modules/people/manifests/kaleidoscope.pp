eac8d97922bcd743c8ceddde8a5809a5720887c8
Lance Speelmon <lance@speelmon.com>
2012-03-21 12:11:47 -0700

    ACAD-722 fix up ssh permission issues with pukey authn (~kaledoscope cannot be group writable!) ugh

--- modules/people/manifests/kaleidoscope.pp
+++ modules/people/manifests/kaleidoscope.pp
@@ -17,6 +17,7 @@ class people::kaleidoscope::external {
     realize(Ssh_authorized_key['kaleidoscope-pub'])
     realize(Ssh_authorized_key['kaleidoscope-pub-real'])
     realize(Ssh_authorized_key['kaleidoscope-pub-real2'])
+    realize(Ssh_authorized_key['kaleidoscope-rsmart-preview-pub'])
 }
 
 class people::kaleidoscope {
@@ -69,6 +70,13 @@ class people::kaleidoscope {
         ensure => present,
         key  => 'AAAAB3NzaC1kc3MAAACBAJrlvqcHHmF2xPNxrsINm+RqMe1AF5DTyYUVE0PzAejRBy7/7L8fI1nBkyrQ8Qi6l8gPkna8nhlVF6FmitFf/zEWIH7MAV22mso+ZNJ2ZVvMGGY0VUL82sQyHWL5Pt2HnBoQbKfxqruXvqPfmXQ/eeRfqazF2IVeanytvE/ggPMLAAAAFQCJNqakjbPlUraXIqu8H5Zhcp2HIwAAAIAg8jn5Vt3n4xHNQlrUax6GOtm2IXa3NgbCKx7UhCRwIB7K5pw6vV0LyrIxf+sVrFar0Ob/BURSyh4xE/4N85q2sVJiuBFXaKG612IMgZ+AaBXEGUE88KB2itU66nxy5qW4VVtDjT0GH7WXbpswwbOE1DQ6rhpjmfFVHKd1Eyi3UAAAAIA4oW2teLwPFWAgjIdRv2VvrI85PL7UCfXy0Kp0r4AUfcHPDi7FEmYI4QzzJf9r90lX4e2bDnqcxgiWhFuITP0nOCS4Tqf4zojU5DJEshUXlKJM+3ZphAtaOCZWyfl7l19PXHGFO0MCQtT7hdHjJ23kK8/QhkFFfv96KIfC8lh9Rg==',
         type => 'ssh-dss',
+        user => 'kaleidoscope',
+        require => User['kaleidoscope'],
+    }
+    @ssh_authorized_key { 'kaleidoscope-rsmart-preview-pub':
+        ensure => present,
+        key  => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAzf5fTjX14VZ3cukhr7Fx/adRx2btms0/FQHcqtLywg05sHG8Kao5zCL/oprXQ+moKipBDeEujmJ2WDpbweaWBPaADNRSHm0Qly5sMGTz2erK7e/lRxGpx7GVcqVT/eoVMq7CZSp+i4mr0rp9H/nyxpEP1Dpltb5PtEup0ECV7oNxnfJgF6Rr8lwXp6mOUq3mw78Hkbil6bdtG1NHq2L6+x5j7nIt8OVd2Mtb0CVZ7hdaAojNDy2BWCq5OrLbuheYRakUOWZfIFAWyN7jDvOWAM1Bu0pQ2QgpyodS2v0g+t8HYABQVO7By55+uYqAbJOwwDqYIfJq5KqNhGy3DnIacw==',
+        type => 'ssh-rsa',
         user => 'kaleidoscope',
         require => User['kaleidoscope'],
     }

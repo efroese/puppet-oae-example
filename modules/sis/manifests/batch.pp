84a5f67c6559c2d292da2175c7d70af88a0ef63f
Erik Froese <erik.froese@gmail.com>
2012-03-23 15:21:58 -0400

    NOJIRA Don't archive csv files.

--- modules/sis/manifests/batch.pp
+++ modules/sis/manifests/batch.pp
@@ -16,8 +16,6 @@
 #
 # $school_properties:: A hash that maps school name to its properties overrides
 #
-# $archive_csv_files:: Save csv files from failed runs. '1' is true, anything else is false.
-#
 # == Sample Usage:
 #
 # class { 'sis::batch':
@@ -51,8 +49,7 @@ class sis::batch (
     $csv_object_types,
     $csv_dir        = false,
     $school_properties = { 'not configured' => { 'not' => 'configured'}, },
-    $email_report,
-    $archive_csv_files='0'
+    $email_report
     ) inherits sis {
 
     file { "${sis::basedir}/batch":

8fdc07df4a2291fccff97c6576cfb5607b04f5b3
Kyle Campos <kyle.campos@gmail.com>
2012-04-04 14:57:26 -0700

    the rsmartian-deploy pub key wasn't being realized for rsmart user
    needed for capistrano

--- modules/people/manifests/users.pp
+++ modules/people/manifests/users.pp
@@ -14,7 +14,7 @@ class people::users {
 		managehome => true,
 	}
 		
-	@ssh_authorized_key { "${localconfig::user}-pub":
+	@ssh_authorized_key { "${localconfig::user}-rsmartian-deploy-pub":
         ensure => present,
         key  => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAxcIV32jkMIEEfTVWHsDxNuUHHwaXMWviRAmU7dShSVw3BeMVDsh1syd54p1TRTKICD/9hlVtFDS0MQ7WEyNWUsC/XCO5vSkct5NCsivD1NO7g+Do3V6p+Hzj3Ja18GOjKBU+wi3QVIiyMFhTCqdM1VR9AT/1lPFBxPG+3RWRiwCn7w4YjfumGrDBLJsQh0cCs46kXV3F78VfLOR1QwECiyICBtP77Qzusq2wlVOBolSGrvytXLYDJs5WgdlpU1lSJsr4vHD2WjTicyP8dMbwZAXRUsB4YIT5k7blsEj1iezdZ5EntEvOgDFyWchIF8kzw+LmVw90aLTZco/kyeJYXw==',
         type => 'ssh-rsa',

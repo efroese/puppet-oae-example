ee69d3493fd0efb647e59e8812534fbce4e17092
Erik Froese <erik.froese@gmail.com>
2012-04-25 16:50:43 -0400

    ACAD-978 Fix socket.timeout config key.

--- modules/rsmart-common/manifests/clustered/acad-nodes.pp
+++ modules/rsmart-common/manifests/clustered/acad-nodes.pp
@@ -186,7 +186,7 @@ node oaeappnode inherits oaenode {
         "org.sakaiproject.nakamura.solr.RemoteSolrClient":
         config => {
             "remoteurl"  => $localconfig::solr_remoteurl,
-            "socket-timeout" => 10000,
+            "socket.timeout" => 10000,
             "connection.timeout" => 3000,
             "max.total.connections" => 500,
             "max.connections.per.host" => 500,

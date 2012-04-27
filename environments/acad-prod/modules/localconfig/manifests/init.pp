3137691b8e85f6686e6d1563df9927dc5cae054f
Lance Speelmon <lance@speelmon.com>
2012-04-26 22:32:46 -0700

    NOJIRA Cannot reassign variable tomcat_user at /home/rsmartian/puppet-oae/environments/acad-prod/modules/localconfig/manifests/init.pp:103 on node apache1.academic.rsmart.local

--- environments/acad-prod/modules/localconfig/manifests/init.pp
+++ environments/acad-prod/modules/localconfig/manifests/init.pp
@@ -100,9 +100,6 @@ class localconfig {
     $reply_as_address = 'noreply@rsmart.com'
     $reply_as_name = 'rSmart'
 
-    $tomcat_user    = 'admin'
-    $tomcat_password = 't0msm@rt!'
-
     class extra_users {
         realize(Group['karagon'])
         realize(User['karagon'])

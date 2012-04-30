8671e4eb153251bd534b375c67d79333834853c1
Erik Froese <erik@nyu.edu>
2012-04-30 19:43:58 -0400

    Pass params from solr::tomcat to solr:common.

--- manifests/jetty.pp
+++ manifests/jetty.pp
@@ -1,6 +1,6 @@
 # = Class: solr
 #
-# This class installs a standalone solr master or slave with jetty.
+# This class installs a standalone solr master or slave running in the Jetty servlet container.
 #
 # == Parameters:
 #
@@ -23,6 +23,7 @@
 #   }
 #
 class solr::jetty(
+    $basedir           = '/usr/local/solr',
     $user              = 'root',
     $group             = 'root',
     $solr_tarball      = "http://nodeload.github.com/sakaiproject/solr/tarball/master",
@@ -30,15 +31,21 @@ class solr::jetty(
     $solrconfig        = 'solr/solrconfig.xml.erb',
     $master_url        = 'set the master url' ) {
 
+    # Make sure solr::common is executed BEFORE solr::jetty
     Class['solr::common'] -> Class['solr::jetty']
 
+    # Lift heavy things
     class { 'solr::common':
+        basedir           => basedir,
+        user              => $user,
+        group             => $group,
         solr_tarball      => $solr_git,
         solr_home_tarball => $solr_home_tarball,
         solrconfig        => $solrconfig,
         master_url        => $master_url,
     }
 
+    # Drop the init script
     file { '/etc/init.d/solr':
         ensure => present,
         owner  => $user,
@@ -47,6 +54,7 @@ class solr::jetty(
         content => template("solr/solr.erb"),
     }
 
+    # And turn it on
     service { 'solr':
         ensure => running,
         enable => true,

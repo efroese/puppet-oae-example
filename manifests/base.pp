09ce42d5b1bff382e1693ab25a6b74b08387087d
Erik Froese <erik.froese@gmail.com>
2012-06-05 09:43:41 -0400

    Squashed 'modules/augeas/' content from commit 1afd076
    git-subtree-dir: modules/augeas
    git-subtree-split: 1afd076a152f43645c91b1e2bab15fda7498055c

---
+++ manifests/base.pp
@@ -0,0 +1,34 @@
+class augeas::base {
+  $lens_dir = '/usr/share/augeas/lenses/contrib'
+
+  $version = $augeas_version ? {
+    ''      => 'present',
+    default => $augeas_version
+  }
+
+  $rubylib_version = $augeas_ruby_version ? {
+    ''      => 'present',
+    default => $augeas_ruby_version
+  }
+
+  # ensure no file not managed by puppet ends up in there.
+  file { $lens_dir:
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    force   => true,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+  file { "${lens_dir}/tests":
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    force   => true,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
+}

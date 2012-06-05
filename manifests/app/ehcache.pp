0e966cf2aa8a6d2ccd50362581d0166b054f07bf
Erik Froese <erik.froese@gmail.com>
2012-06-05 09:46:48 -0400

    Squashed 'modules/oae/' content from commit 75c6bd4
    git-subtree-dir: modules/oae
    git-subtree-split: 75c6bd467a277f197b9ddbb54f818e589b02240e

---
+++ manifests/app/ehcache.pp
@@ -0,0 +1,72 @@
+# = Class: oae::app::ehcache
+#
+# Configure ehcache replicated to OAE app servers in a cluster
+#
+# == Parameters:
+#
+# $config_xml::     A template path to rander the ehcacheConfig.xml file
+#
+# $mcast_address::  The multicast address to use for cluster communication
+#
+# $mcast_port::     The multicast port to use for cluster communication
+#
+# $peers::          A list of peers. Required only for tcp communication
+#
+# $tcp_address::    TCP address for cluster communication
+#
+# $tcp_port::       TCP port for cluster communication
+#
+# $remote_object_port:: Secondary TCP port for cluster communication
+#
+# == Actions:
+#   Configure ehCache on an OAE server
+#
+# == Sample Usage:
+# 
+#   class { 'oae::app::ehcache':
+#     config_xml    => 'localconfig/ehcacheConfig.xml.erb'
+#     mcast_address => '224.0.0.1',
+#     mcast_port    => '5509',
+#   }
+#
+#   # Some environments don't allow multicast. Use tcp instead.
+#   class { 'oae::app::ehcache':
+#     config_xml  => 'localconfig/ehcacheConfig.xml.erb'
+#     peers       => [ '192.168.1.50', '192.168.1.51' ]
+#     tcp_address => '192.168.1.50',
+#     tcp_port    => '40001',
+#     remote_object_port => '40002',
+#   }
+class oae::app::ehcache ($config_xml = 'oae/ehcacheConfig.xml.erb',
+                         $peers=[],
+                         $tcp_address = '',
+                         $tcp_port = '40001',
+                         $remote_object_port = '',
+                         $mcast_address = '230.0.0.2',
+                         $mcast_port = '8450') {
+
+    Class['oae::params'] -> Class['oae::app::ehcache']
+
+    # OAE 1.1
+    $replicated_caches = [
+        'org.sakaiproject.nakamura.auth.trusted.TokenStore',
+        'presence.location',
+        'presence.status',
+        'deletedPathQueue',
+        'accessControlCache',
+        'authorizableCache',
+        'contentCache',
+        'lockmanager.lockmap',
+        'server-tracking-cache', ]
+
+    if $tcp_address != '' {
+        $rmiurls = template('oae/rmiurls.erb')
+    }
+
+    file { "${oae::params::basedir}/sling/ehcacheConfig.xml":
+        owner => $oae::params::user,
+        group => $oae::params::group,
+        mode  => 0440,
+        content => template($config_xml),
+    }
+} 
---
+++ manifests/preview_processor/openoffice.pp
@@ -0,0 +1,38 @@
+# = Class: oae::preview_processor::openoffice
+#
+# Install openoffice for the preview processor
+#
+class oae::preview_processor::openoffice {
+
+    if $operatingsystem =~ /Amazon|Linux/ {
+        $pkg_string = 'openoffice.org-core.x86_64 openoffice.org-javafilter.x86_64 openoffice.org-headless.x86_64 openoffice.org-base-core.x86_64 openoffice.org-calc-core.x86_64 openoffice.org-draw-core.x86_64 openoffice.org-math-core.x86_64 openoffice.org-writer-core.x86_64 openoffice.org-brand.x86_64'
+        exec { 'install-ooo-centos':
+            command => "yum -y --enablerepo=centos6-base install ${pkg_string}",
+            unless  => "rpm -q ${pkg_string}",
+            notify => File['/usr/lib/openoffice'],
+        }
+    }
+    else {
+        package { [ 'openoffice.org-core',
+                    'openoffice.org-brand',
+                    'openoffice.org-javafilter',
+                    'openoffice.org-headless',
+                    'openoffice.org-base-core',
+                    'openoffice.org-calc-core',
+                    'openoffice.org-draw-core',
+                    'openoffice.org-math-core',
+                    'openoffice.org-writer-core' ]:
+            ensure => installed,
+            notify => File['/usr/lib/openoffice'],
+        }
+    }
+    # Create Link /usr/lib/openoffice
+    file { '/usr/lib/openoffice':
+        ensure => link,
+        target => '/usr/lib64/openoffice.org3',
+        require => $operatingsystem ? {
+            /Amazon|Linux/ => Exec['install-ooo-centos'],
+            default => Package['openoffice.org-core'],
+        }
+    }
+}

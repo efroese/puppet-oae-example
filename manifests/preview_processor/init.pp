b8ad60d646624c0d380f9abe80f5d0fb622b2ca0
Erik Froese <erik.froese@gmail.com>
2012-07-05 09:23:04 -0400

    Move the preview processor logs to /var/log/sakaioae/preview

--- manifests/preview_processor/init.pp
+++ manifests/preview_processor/init.pp
@@ -58,6 +58,30 @@ class oae::preview_processor::init (
         timeout => 0,
     }
 
+    # /usr/local/sakaioae/nakamura/scripts/logs -> '/var/log/sakaioae/preview'
+    # If /usr/local/sakaioae/nakamura/scripts/logs is not a symlink, move it to
+    # /var/log/sakaioae/preview and then let puppet fix up the perms
+    $log_dir = '/var/log/sakaioae/preview'
+    exec { 'mv preview logs':
+        command => "mv ${oae::params::basedir}/nakamura/scripts/logs ${log_dir}",
+        unless  => "test -L ${oae::params::basedir}/nakamura/scripts/logs",
+    }
+
+    file { $log_dir:
+        ensure  => directory,
+        owner   => root,
+        group   => $oae::params::group,
+        mode    => 0775,
+        require => [ Exec['mv nakamura'], Exec['mv preview logs'], ],
+    }
+
+    file { "${oae::params::basedir}/nakamura/scripts/logs":
+        ensure  => link,
+        target  => '/var/log/sakaioae/preview',
+        require => File['/var/log/sakaioae/preview'],
+    }
+
+    # OAE password file
     file { "${oae::params::basedir}/.oae_credentials.txt":
         owner => $oae::params::user,
         group => $oae::params::group,
@@ -65,16 +89,21 @@ class oae::preview_processor::init (
         content => "${upload_url} ${admin_password}",
     }
 
-    ###########################################################################
-    # Drop the script for the cron job
+    # Script for the cron job
     file { "${oae::params::basedir}/bin/run_preview_processor.sh":
         content => template('oae/run_preview_processor.sh.erb'),
         owner  => root,
         group  => root,
         mode   => 755,
     }
-    
-    $full_os = "${operatingsystem}${lsbmajdistrelease}"
+
+    # Nagios check
+    file { "${oae::params::basedir}/bin/check_preview_processor.sh":
+        content => template('oae/check_preview_processor.sh.erb'),
+        owner  => root,
+        group  => root,
+        mode   => 755,
+    }
 
     cron { 'run_preview_processor':
         command => "${oae::params::basedir}/bin/run_preview_processor.sh 2>&1 > /dev/null",

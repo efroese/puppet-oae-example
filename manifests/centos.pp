cfb55039907e4c581e8b922d3d7891c2560c184d
Erik Froese <erik.froese@gmail.com>
2012-07-16 10:23:11 -0400

    Use the mirrorlist param for a mirrorlist.

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -4,7 +4,7 @@ class centos {
 
     yumrepo { 'centos6-base':
         name       => 'centos6-base',
-        baseurl    => "http://mirrorlist.centos.org/?release=6&arch=${architecture}&repo=os",
+        mirrorlist => "http://mirrorlist.centos.org/?release=6&arch=${architecture}&repo=os",
         gpgcheck   => '1',
         gpgkey     => 'http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6',
         priority   => '99',

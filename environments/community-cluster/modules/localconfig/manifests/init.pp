9013634d57a755fd549ee7d2cfe5a6cf0e5658a9
Erik Froese <erik.froese@gmail.com>
2012-07-16 16:16:18 -0400

    lowercase the http elb names. remove unused variables.

--- environments/community-cluster/modules/localconfig/manifests/init.pp
+++ environments/community-cluster/modules/localconfig/manifests/init.pp
@@ -28,12 +28,14 @@ class localconfig {
     $app_server0 = 'ip-10-176-227-221.us-west-1.compute.internal'
     $app_server1 = 'ip-10-168-249-50.us-west-1.compute.internal'
 
-    $app_server0_ip = dnsLookup($localconfig::app_server0)
-    $app_server1_ip = dnsLookup($localconfig::app_server1)
+    # These will eventually change.
+    $app_server0_ip = dnsLookup($app_server0)
+    $app_server1_ip = dnsLookup($app_server1)
 
     # ELBs for trusted and untrusted content
-    $http_name              = 'OAE-AppServers-365563856.us-west-1.elb.amazonaws.com'
-    $http_name_untrusted    = 'OAE-AppServers-Untrusted-414965918.us-west-1.elb.amazonaws.com'
+
+    $http_name              = 'oae-appservers-365563856.us-west-1.elb.amazonaws.com'
+    $http_name_untrusted    = 'oae-appservers-untrusted-414965918.us-west-1.elb.amazonaws.com'
 
     $javamemorymax = '512'
     $javamemorymin = '256'
@@ -45,9 +47,7 @@ class localconfig {
     # Solr
     $solr_master = 'OAE-SOLR-426995740.us-west-1.elb.amazonaws.com'
     $solr_remoteurl = "http://${solr_master}:8080/solr"
-    $solr_queryurls = "http://${solr_master}:8080/solr"
 
     # EHcache
-    $ehcache_tcp_port = '40001'
     $ehcache_remote_object_port = '40002'
 }
